4cacc4766c94b2402bd035e161e93e7f494569e5
eddyv <vaismanedward@gmail.com>
2021-05-01 02:29:16 -0400

    give feedback when user is logging in

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,8 +5,11 @@ import "./Login.css";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../libs/contextLib";
 import { useHistory } from "react-router";
+import LoaderButton from "../components/LoaderButton";
+import { onError } from "../libs/errorLib";
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
   const { userHasAuthenticated } = useAppContext();
   const [email, setEmail] = useState("");
@@ -19,12 +22,15 @@ export default function Login() {
   async function handleSubmit(event) {
     event.preventDefault();
 
+    setIsLoading(true);
+
     try {
       await Auth.signIn(email, password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
-      alert(e.message);
+      onError(e);
+      setIsLoading(false);
     }
   }
 
@@ -50,9 +56,15 @@ export default function Login() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
+        <LoaderButton
+          block
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </Form>
     </div>
   );
