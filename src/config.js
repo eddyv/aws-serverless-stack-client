61c4b7878b6b63b51dbb19e44f6dc6890f9c2089
eddyv <vaismanedward@gmail.com>
2021-05-02 15:16:31 -0400

    added stripe billing form

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config = {
+  STRIPE_PUB_KEY: "pk_test_51FqADkAgS18VkVURqmaWRzO3MSzOrxEDfCo0AMnTzb1I6DFrdlHKv9z01jhG84WAjLjUopnFsFp2ZvzC99Vk68mI009stpoXHX",
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
